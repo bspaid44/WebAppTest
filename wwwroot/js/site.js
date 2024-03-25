8091c853e3d6764f5e192754c963052993700796
bspaid44 <brandonspaid@gmail.com>
2024-03-25 11:10:01 -0400

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,6 @@
+﻿var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene, {
+    clipRelativeInput: true,
+    relativeInput: true,
+});
+parallaxInstance.friction(0.15, 0.15).calibrate(x, y);
