1e2309873202c1124959656aa063fea3ce6648a7
bspaid44 <brandonspaid@gmail.com>
2024-03-25 13:54:33 -0400

    added hover only effect

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,6 +1,10 @@
 ﻿var scene = document.getElementById('scene');
+var x = window.innerWidth / 2;
+var y = window.innerHeight / 2;
+
 var parallaxInstance = new Parallax(scene, {
     clipRelativeInput: true,
     relativeInput: true,
 });
 parallaxInstance.friction(0.15, 0.15).calibrate(x, y);
+
