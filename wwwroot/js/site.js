904f54cf3bea64d0ca2e5fa50cf515343ae90022
bspaid44 <brandonspaid@gmail.com>
2024-03-25 20:34:17 -0400

    Added third parallax, updated credits

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,7 +1,6 @@
 ﻿var scene = document.getElementById('scene');
 var scene1 = document.getElementById('scene1');
-var x = window.innerWidth / 2;
-var y = window.innerHeight / 2;
+var scene2 = document.getElementById('scene2');
 
 var parallaxInstance = new Parallax(scene, {
     clipRelativeInput: true,
@@ -14,4 +13,10 @@ var parallaxInstance1 = new Parallax(scene1, {
     relativeInput: true,
 });
 parallaxInstance1.friction(0.15, 0.15);
+
+var parallaxInstance2 = new Parallax(scene2, {
+    clipRelativeInput: true,
+    relativeInput: true,
+});
+parallaxInstance2.friction(0.15, 0.15);
 
