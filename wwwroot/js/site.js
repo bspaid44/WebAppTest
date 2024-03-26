b039e0abeb28d933709714d4f1064a2695baa300
bspaid44 <brandonspaid@gmail.com>
2024-03-25 20:07:22 -0400

    added second parallax image

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,5 @@
 ﻿var scene = document.getElementById('scene');
+var scene1 = document.getElementById('scene1');
 var x = window.innerWidth / 2;
 var y = window.innerHeight / 2;
 
@@ -6,5 +7,11 @@ var parallaxInstance = new Parallax(scene, {
     clipRelativeInput: true,
     relativeInput: true,
 });
-parallaxInstance.friction(0.15, 0.15).calibrate(x, y);
+parallaxInstance.friction(0.15, 0.15);
+
+var parallaxInstance1 = new Parallax(scene1, {
+    clipRelativeInput: true,
+    relativeInput: true,
+});
+parallaxInstance1.friction(0.15, 0.15);
 
